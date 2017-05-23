497451a9f68e647afbc09533695431d6558b457c
Leonardo Castro <leonardo.castro@mail.escuelaing.edu.co>
2017-05-23 14:01:01 -0500

    Configuracion clases y dependencias

--- src/main/java/msgbroker/WebsocketsMsbrokerExampleApplication.java
+++ src/main/java/msgbroker/WebsocketsMsbrokerExampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebsocketsMsbrokerExampleApplication {
 
-	public static void main(String[] args) {
+	public static void mains(String[] args) {
             
 		SpringApplication.run(WebsocketsMsbrokerExampleApplication.class, args);
 	}
